d8f0cf3eea144fb7554658e7e523ce79e11d43d0
Daniele Albanese <daniele.albanese98@gmail.com>
2020-07-20 21:09:24 +0200

    app.py -> Changed base API

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return "It working..."
+    return make_response(True, "It works...", 200)
 
 
 @app.route("/signup", methods=["POST"])
